68967c7c6176d72056722543e79a22e69e7a3804
Clarice Yuen <cyuen025@gmail.com>
2025-04-05 15:46:15 -0700

    Update API call to https

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const autoCompleteConfig = {
     return movie.Title;
   },
   async fetchData(searchTerm) {
-    const response = await axios.get('http://www.omdbapi.com/', {
+    const response = await axios.get('https://www.omdbapi.com/', {
       params: {
         apikey: '9e54abf4',
         s: searchTerm,
@@ -46,7 +46,7 @@ createAutoComplete({
 let leftMovie;
 let rightMovie;
 const onMovieSelect = async (movie, summaryElement, side) => {
-  const response = await axios.get('http://www.omdbapi.com/', {
+  const response = await axios.get('https://www.omdbapi.com/', {
     params: {
       apikey: '9e54abf4',
       i: movie.imdbID,
